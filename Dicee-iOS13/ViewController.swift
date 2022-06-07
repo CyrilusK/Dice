5c6601309b4719f998ace5301888aa54cfcb60a4
CyrilusK <kardash199696@mail.ru>
2022-06-08 01:44:38 +0300

    adding project

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let arrOfImagesDice = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var firstDiceImage: UIImageView!
+    @IBOutlet weak var secondDiceImage: UIImageView!
+    @IBOutlet weak var keyPressRoll: UIButton!
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        keyPressRoll.alpha = 0.5
+        firstDiceImage.alpha = 0
+        secondDiceImage.alpha = 0
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) { [self] in
+            keyPressRoll.alpha = 1
+            firstDiceImage.alpha = 1
+            secondDiceImage.alpha = 1
+            firstDiceImage.image = arrOfImagesDice.randomElement()
+            secondDiceImage.image = arrOfImagesDice.randomElement()
+        }
     }
-
-
 }
 
